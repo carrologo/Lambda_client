ea1d679dfa09b90d0a7a6a261a701ddf47a642f5
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-04-16 12:06:00 -0500

    feac(first-commit)
    Se añade la configuración

---
+++ index.js
@@ -0,0 +1,10 @@
+import {format} = require('date-fns');
+
+
+export const handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(`Hello from Lambda! ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`),
+  };
+  return response;
+};
