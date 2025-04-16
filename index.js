b4382228cea735e4f2ad2bc20338316f373812f3
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-04-16 13:49:34 -0500

    se corrije la importación de la libreria

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { format } from 'date-fns';
+const { format } = require('date-fns');
 
-export const handler = async (event) => {
+exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify(`Hello from Lambda! ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`),
